1349abf45b157cbb964597e63020e5529cc14b19
Vepp1 <vinipontarolla@gmail.com>
2022-12-04 17:33:05 +0000

    Add snippets cheatshheet

---
+++ my-app/src/components/ES7Snippets.js
@@ -0,0 +1,48 @@
+// import React from 'react' -- rfce
+
+// function ES7Snippets() {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets
+
+
+
+// import React from 'react' -- rafce
+
+// const ES7Snippets = () => {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets
+
+// import React, { Component } from 'react' -- rce
+
+// export class ES7Snippets extends Component {
+//   render() {
+//     return (
+//       <div>ES7Snippets</div>
+//     )
+//   }
+// }
+
+// export default ES7Snippets
+
+
+
+// constructor(props) { -- rconst
+//   super(props)
+
+//   this.state = {
+//      first
+//   }
+// }
+
+
+// console.log(first) -- clg
+
+// console.log('first', first) -- clo
